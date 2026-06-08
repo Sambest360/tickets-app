06546d5ff6f2ef2aa8b05ff05b44ff8807169f7d
Samuel Koranteng Amoah Bampo <sambampo200@gmail.com>
2026-06-08 16:30:28 +0200

    vercal update 2

--- artifacts/api-server/build.mjs
+++ artifacts/api-server/build.mjs
@@ -15,7 +15,10 @@ async function buildAll() {
   await rm(distDir, { recursive: true, force: true });
 
   await esbuild({
-    entryPoints: [path.resolve(artifactDir, "src/index.ts")],
+    entryPoints: [
+      path.resolve(artifactDir, "src/index.ts"),
+      path.resolve(artifactDir, "src/app.ts"),
+    ],
     platform: "node",
     bundle: true,
     format: "esm",
@@ -103,8 +106,10 @@ async function buildAll() {
     ],
     sourcemap: "linked",
     plugins: [
-      // pino relies on workers to handle logging, instead of externalizing it we use a plugin to handle it
-      esbuildPluginPino({ transports: ["pino-pretty"] })
+      esbuildPluginPino({
+        transports:
+          process.env.NODE_ENV === "production" ? [] : ["pino-pretty"],
+      }),
     ],
     // Make sure packages that are cjs only (e.g. express) but are bundled continue to work in our esm output file
     banner: {
